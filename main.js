b171abb3959889c4055939958aaae0878a48c132
TINHOLINDO <134516651+TINHOLINDO@users.noreply.github.com>
2024-08-09 10:38:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector('.caixa principal');
+const caixaPergunta = document.querySelector('caixa-pergunta');
+const caixaAlternativa = document.querySelector('caixa-alternativa');
+const caixaResultado = document.querySelector('caixa-reultado');
+const textoResultado = document.querySelector('texto-resultado');
+
+const perguntas = [
+{
+    enunciado: 'texto01',
+    alternativas: [
+        "alternativa01",
+        "alternativa02"
+        ]
+    }
+]
